e6738135ce99b7e07d480a351447f23b2ace2fbb
Jorge Florian <jafn28@gmail.com>
2018-10-03 09:18:51 -0500

    remove setupTests configuration

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose')
 const Migration = require('./migration.model')
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+mongoose.connection.on('error', function (err) {
+  console.error('MongoDB connection error: ' + err)
+  process.exit(1)
+})
 
 // determinate default value
 const serverMigrationPath = () => process.env.SERVER_MIGRATION_PATH || `${__dirname}/migrations`
--- setupTests.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.Promise = Promise
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-
-mongoose.connection.on('error', function (err) {
-  console.error('MongoDB connection error: ' + err)
-  process.exit(1)
-})
