6f4b8230a23a24fd2dae3c0ac29b4e8b684a2bf3
Don Schrimsher <don@noomatech.com>
2018-10-24 13:54:03 -0600

    Throw error when seed path is missing

--- __tests__/seed.test.js
+++ __tests__/seed.test.js
@@ -1,3 +1,6 @@
+const jsonfile = require("jsonfile");
+const path = require("path");
+
 const Todo = require("./support/models/todo");
 const { execSync } = require("./support/utils");
 
@@ -20,4 +23,24 @@ test("seeding the database with no MONGODB_URI", () => {
   }).toThrowError(
     "No `MONGODB_URI` environment variable found. Please specify the mongo connection string in the `MONGODB_URI` environment variable."
   );
+});
+
+test("seeding the database with a missing seed path", async () => {
+  const packageJsonPath = path.resolve(__dirname, "../", "package.json");
+  const originalPackageJson = jsonfile.readFileSync(packageJsonPath);
+  const packageJsonWithMissingSeedPath = {
+    ...originalPackageJson,
+    db: { ...originalPackageJson.db, seedPath: null }
+  };
+  jsonfile.writeFileSync(packageJsonPath, packageJsonWithMissingSeedPath, {
+    spaces: 2
+  });
+
+  expect(() => {
+    execSync("bin/db seed");
+  }).toThrowError(
+    "No seed path specified. Please specify a path relative to your root directory that contains your seed file."
+  );
+
+  jsonfile.writeFileSync(packageJsonPath, originalPackageJson, { spaces: 2 });
 });
