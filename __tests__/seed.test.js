aeaec0a7e281da7decb5db963f08434ddb2be329
Don Schrimsher <don@noomatech.com>
2018-10-24 12:43:58 -0400

    Throw error when missing MONGODB_URI
    Throws an error in the seed executable when the MONGODB_URI is missing.

--- __tests__/seed.test.js
+++ __tests__/seed.test.js
@@ -1,13 +1,23 @@
 const Todo = require("./support/models/todo");
-const { execSync } = require('./support/utils')
+const { execSync } = require("./support/utils");
 
 beforeEach(async () => {
   await Todo.deleteMany({});
 });
 
-test("seeding a database", async () => {
-  execSync('bin/db seed')
+test("seeding the database", async () => {
+  execSync("bin/db seed");
 
   const results = await Todo.find();
   expect(results).toHaveLength(1);
+});
+
+test("seeding the database with no MONGODB_URI", () => {
+  delete process.env.MONGODB_URI;
+
+  expect(() => {
+    execSync("bin/db seed");
+  }).toThrowError(
+    "No `MONGODB_URI` environment variable found. Please specify the mongo connection string in the `MONGODB_URI` environment variable."
+  );
 });
